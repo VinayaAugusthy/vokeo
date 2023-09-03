00845e2322c204cedadf83631b812b683459a5d5
VinayaAugusthy <vinayaaugusthy@gmail.com>
2023-09-03 11:11:12 +0530

    methods extracted

--- lib/screens/home/widgets/post_card.dart
+++ lib/screens/home/widgets/post_card.dart
@@ -1,4 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
@@ -20,65 +21,17 @@ class PostCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    final postCardProvider = Provider.of<PostCardProvider>(context);
     final user = Provider.of<UserProvider>(context).getUser;
-
-    final isLikeAnimating = postCardProvider.isLikeAnimating;
-    final commentCount = postCardProvider.commentCount;
 
     return Container(
       color: Colors.black,
       padding: EdgeInsets.symmetric(
-        vertical: size.width * 0.005,
-        horizontal: size.width * 0.005,
+        vertical: MediaQuery.of(context).size.width * 0.005,
+        horizontal: MediaQuery.of(context).size.width * 0.005,
       ),
       child: Column(
         children: [
-          Container(
-            padding: const EdgeInsets.symmetric(
-              vertical: 4,
-              horizontal: 16,
-            ).copyWith(right: 0),
-            child: Row(
-              children: [
-                CircleAvatar(
-                  radius: 16,
-                  backgroundImage: NetworkImage(snap['profileImage']),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 8),
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        GestureDetector(
-                          onTap: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  ProfileScreen(uid: snap['uid']),
-                            ),
-                          ),
-                          child: Text(
-                            snap['username'],
-                            style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white),
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-
-                //====================Edit deletepost===========================================
-                snap['uid'] == FirebaseAuth.instance.currentUser!.uid
-                    ? editOrDeletePosts(context)
-                    : const SavePosts(),
-              ],
-            ),
-          ),
+          buildHeader(context),
           GestureDetector(
             onDoubleTap: () async {
               await FirestoreMethods().likePost(
@@ -87,168 +40,250 @@ class PostCard extends StatelessWidget {
                 snap['likes'],
               );
             },
-            child: Stack(
-              alignment: Alignment.center,
-              children: [
-                GestureDetector(
-                  onTap: () => Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) => ViewPost(postUrl: snap['postUrl']),
-                    ),
-                  ),
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height * 0.35,
-                    width: double.infinity,
-                    child: Image(
-                      image: NetworkImage(snap['postUrl']),
-                      fit: BoxFit.cover,
-                    ),
+            child: buildImageStack(context),
+          ),
+          buildActionRow(context),
+          buildPostInfo(context),
+        ],
+      ),
+    );
+  }
+
+  Widget buildHeader(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 4,
+        horizontal: 16,
+      ).copyWith(right: 0),
+      child: Row(
+        children: [
+          buildAvatar(),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 8),
+              child: GestureDetector(
+                onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => ProfileScreen(uid: snap['uid']),
                   ),
                 ),
-                AnimatedOpacity(
-                  duration: const Duration(milliseconds: 200),
-                  opacity: isLikeAnimating ? 1 : 0,
-                  child: LikeAnimation(
-                    isAnimating: isLikeAnimating,
-                    duration: const Duration(milliseconds: 400),
-                    child: const Icon(
-                      Icons.favorite,
-                      color: Colors.red,
-                      size: 100,
-                    ),
+                child: Text(
+                  snap['username'],
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
-                )
-              ],
+                ),
+              ),
             ),
           ),
-          Row(
-            children: [
-              LikeAnimation(
-                isAnimating: snap['likes'].contains(user!.uid),
-                smallLike: true,
-                child: IconButton(
-                  onPressed: () async {
-                    await FirestoreMethods().likePost(
-                      user.uid,
-                      snap['postId'],
-                      snap['likes'],
-                    );
+          buildPopupMenu(context),
+        ],
+      ),
+    );
+  }
 
-                    if (snap['uid'] != FirebaseAuth.instance.currentUser!.uid &&
-                        snap['likes'].contains(user.uid)) {
-                      await FirestoreMethods().showNotifications(
-                          postUrl: snap['postUrl'],
-                          postId: snap['postId'],
-                          text: "liked",
-                          owner: snap['uid'],
-                          name: user.username,
-                          profilePic: user.photoUrl,
-                          uid: FirebaseAuth.instance.currentUser!.uid);
-                    }
-                  },
-                  icon: snap['likes'].contains(user.uid)
-                      ? const Icon(
-                          Icons.favorite,
-                          color: Colors.red,
-                        )
-                      : const Icon(
-                          Icons.favorite_border,
-                          color: Colors.white,
-                        ),
-                ),
+  Widget buildAvatar() {
+    return CircleAvatar(
+      radius: 16,
+      backgroundImage: NetworkImage(snap['profileImage']),
+    );
+  }
+
+  Widget buildPopupMenu(BuildContext context) {
+    return snap['uid'] == FirebaseAuth.instance.currentUser!.uid
+        ? editOrDeletePosts(context)
+        : const SavePosts();
+  }
+
+  PopupMenuButton<int> editOrDeletePosts(BuildContext context) {
+    return PopupMenuButton(
+      color: Colors.black,
+      icon: const Icon(
+        Icons.more_vert,
+        color: Colors.white,
+      ),
+      onSelected: (value) {
+        if (value == 0) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => EditPostScreen(snap: snap),
+            ),
+          );
+        }
+        if (value == 1) {
+          deletePost(snap['postId'].toString(), context);
+        }
+      },
+      itemBuilder: (BuildContext context) {
+        return [
+          const PopupMenuItem(
+            value: 0,
+            child: Text('Edit', style: TextStyle(color: Colors.white)),
+          ),
+          const PopupMenuItem(
+            value: 1,
+            child: Text('Delete', style: TextStyle(color: Colors.white)),
+          ),
+        ];
+      },
+    );
+  }
+
+  Widget buildImageStack(BuildContext context) {
+    final postCardProvider = Provider.of<PostCardProvider>(context);
+
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        GestureDetector(
+          onTap: () => Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => ViewPost(postUrl: snap['postUrl']),
+            ),
+          ),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image(
+              image: NetworkImage(snap['postUrl']),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        AnimatedOpacity(
+          duration: const Duration(milliseconds: 200),
+          opacity: postCardProvider.isLikeAnimating ? 1 : 0,
+          child: LikeAnimation(
+            isAnimating: postCardProvider.isLikeAnimating,
+            duration: const Duration(milliseconds: 400),
+            child: const Icon(
+              Icons.favorite,
+              color: Colors.red,
+              size: 100,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildActionRow(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).getUser;
+    return Row(
+      children: [
+        LikeAnimation(
+          isAnimating: snap['likes'].contains(user!.uid),
+          smallLike: true,
+          child: IconButton(
+            onPressed: () async {
+              await FirestoreMethods().likePost(
+                user.uid,
+                snap['postId'],
+                snap['likes'],
+              );
+
+              if (snap['uid'] != FirebaseAuth.instance.currentUser!.uid &&
+                  snap['likes'].contains(user.uid)) {
+                await FirestoreMethods().showNotifications(
+                  postUrl: snap['postUrl'],
+                  postId: snap['postId'],
+                  text: "liked",
+                  owner: snap['uid'],
+                  name: user.username,
+                  profilePic: user.photoUrl,
+                  uid: FirebaseAuth.instance.currentUser!.uid,
+                );
+              }
+            },
+            icon: snap['likes'].contains(user.uid)
+                ? const Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                  )
+                : const Icon(
+                    Icons.favorite_border,
+                    color: Colors.white,
+                  ),
+          ),
+        ),
+        IconButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => CommentScreen(snap: snap),
               ),
-              IconButton(
-                onPressed: () {
-                  DocumentSnapshot<Map<String, dynamic>>? postSnap;
+            );
+          },
+          icon: const Icon(
+            Icons.comment_outlined,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
 
-                  Navigator.of(context).push(
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          CommentScreen(snap: snap, documentSnap: postSnap),
-                    ),
-                  );
-                },
-                icon: const Icon(
-                  Icons.comment_outlined,
-                  color: Colors.white,
-                ),
-              ),
+  Widget buildPostInfo(BuildContext context) {
+    final postCardProvider = Provider.of<PostCardProvider>(context);
 
-              // Expanded(
-              //   child: Align(
-              //     alignment: Alignment.bottomRight,
-              //     child: IconButton(
-              //       onPressed: () {},
-              //       icon: const Icon(
-              //         Icons.bookmark,
-              //         color: Colors.white,
-              //       ),
-              //     ),
-              //   ),
-              // )
-            ],
+    return Container(
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.of(context).size.width * 0.03,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "${snap['likes'].length}  Likes",
+            style: const TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           Container(
-            padding: EdgeInsets.symmetric(
-              // vertical: size.width * 0.01,
-              horizontal: size.width * 0.03,
+            width: double.infinity,
+            padding: const EdgeInsets.only(top: 8),
+            child: RichText(
+              text: TextSpan(
+                style: const TextStyle(
+                  color: Colors.white,
+                ),
+                children: [
+                  TextSpan(
+                    text: snap['username'],
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  TextSpan(
+                    text: "  ${snap['description']}",
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
             ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "${snap['likes'].length}  Likes",
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                  ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: Text(
+                "View all ${postCardProvider.commentCount} comments",
+                style: TextStyle(
+                  color: Colors.grey.shade500,
                 ),
-                Container(
-                  width: double.infinity,
-                  padding: const EdgeInsets.only(top: 8),
-                  child: RichText(
-                    text: TextSpan(
-                      style: const TextStyle(
-                        color: Colors.white,
-                      ),
-                      children: [
-                        TextSpan(
-                          text: snap['username'],
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        TextSpan(
-                          text: "  ${snap['description']}",
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                InkWell(
-                  onTap: () {},
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 4),
-                    child: Text(
-                      "View all $commentCount comments",
-                      style: TextStyle(
-                        color: Colors.grey.shade500,
-                      ),
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.symmetric(vertical: 4),
-                  child: Text(
-                    DateFormat.yMMMd().format(
-                      snap['datePublished'].toDate(),
-                    ),
-                    style: TextStyle(
-                      color: Colors.grey.shade500,
-                    ),
-                  ),
-                ),
-              ],
+              ),
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 4),
+            child: Text(
+              DateFormat.yMMMd().format(
+                snap['datePublished'].toDate(),
+              ),
+              style: TextStyle(
+                color: Colors.grey.shade500,
+              ),
             ),
           ),
         ],
@@ -256,43 +291,9 @@ class PostCard extends StatelessWidget {
     );
   }
 
-  PopupMenuButton<int> editOrDeletePosts(BuildContext context) {
-    return PopupMenuButton(
-        color: Colors.black,
-        icon: const Icon(
-          Icons.more_vert,
-          color: Colors.white,
-        ),
-        onSelected: (value) {
-          if (value == 0) {
-            Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (context) => EditPostScreen(snap: snap),
-              ),
-            );
-          }
-          if (value == 1) {
-            deletePost(snap['postId'].toString(), context);
-          }
-        },
-        itemBuilder: (BuildContext context) {
-          return [
-            const PopupMenuItem(
-              value: 0, //---add this line
-              child: Text('Edit', style: TextStyle(color: Colors.white)),
-            ),
-            const PopupMenuItem(
-              value: 1,
-              child: Text('Delete', style: TextStyle(color: Colors.white)),
-            ),
-          ];
-        });
-  }
-
-  deletePost(String postId, BuildContext context) async {
+  void deletePost(String postId, BuildContext context) async {
     String res = await FirestoreMethods().deletePost(postId);
     if (res == "Deleted") {
-      // ignore: use_build_context_synchronously
       showSnackbar(context, "Post Deleted");
     }
   }
